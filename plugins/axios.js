7b989f8fab1b8b31c6f4dfd8f7e2385e7db91308
itcast-hsian <407775611@qq.com>
2020-04-20 10:51:27 +0800

    新建机票首页

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,9 +2,9 @@ import { Message } from 'element-ui';
 // plugins的所有文件都会导入到项目中，并且是全局的，
 // 类似于在普通的vue项目中的main.js里面的内容，对整个项目都有效
 
-// 拦截axios的错误, 插件必须要要暴露出一个函数
+// 1.拦截axios的错误, 插件必须要要暴露出一个函数
 export default (nuxt) => {
-    // onError在axions请求错误的时候会触发，err是错误的对象
+    // 2.onError在axions请求错误的时候会触发，err是错误的对象
     nuxt.$axios.onError(err => {
         // err.response是错误对象的详细信息
         const {statusCode, message} = err.response.data;
